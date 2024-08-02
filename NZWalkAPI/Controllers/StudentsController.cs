9c7c53c417a9a0894a79197cd8733f737e1abdcd
Raghvendra Singh <raghav12347878@gmail.com>
2024-08-02 15:32:37 +0530

    Added StudentsController and gitignore file

---
+++ NZWalkAPI/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // https://localhost:portnum/api/students/GetAllStudents
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] stunames = new string[] { "Raghav", "Raghvendra", "Happy", "Sandeep" };
+            return Ok(stunames);
+        }
+
+    }
+}
