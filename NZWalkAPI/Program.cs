27feaac0bca40f1b98588d5b6db943db1e1121dc
Raghvendra Singh <raghav12347878@gmail.com>
2024-08-02 17:26:36 +0530

    Added AppDbContext class as a service into program.cs with connectionstring and Run the migration to create the db and tables.

--- NZWalkAPI/Program.cs
+++ NZWalkAPI/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalkAPI.DB;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Added AppdbContext to the Services by providing the connection string, So that it can be used at many controller by DI.
+//Basically below line is adding the AppDBContext to the services and providing the connection string to the AppDBContext.
+builder.Services.AddDbContext<AppDBContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("NzConnStr")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
