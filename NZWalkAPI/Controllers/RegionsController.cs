d6c843965c176fdbec0db8de9b34bfac13826138
raghvendra-imarc <raghvendra.singh@imarcgroup.com>
2024-10-22 00:15:09 +0530

    Implemented Edit and UPdate functionality, Lecture-30 on 22-Oct-24.

--- NZWalkAPI/Controllers/RegionsController.cs
+++ NZWalkAPI/Controllers/RegionsController.cs
@@ -21,10 +21,10 @@ namespace NZWalkAPI.Controllers
 
         [HttpGet]
         public IActionResult GetAllRegion()
-            {
+        {
             var regionsList = _db.Regions.ToList();
-            var regionDto= new List<RegionDTO>();
-            foreach (var region in regionsList) 
+            var regionDto = new List<RegionDTO>();
+            foreach (var region in regionsList)
             {
                 regionDto.Add(new RegionDTO()
                 {
@@ -33,14 +33,12 @@ namespace NZWalkAPI.Controllers
                     Code = region.Code,
                     ImageUrl = region.RegionImageUrl
                 });
-            
             }
             //var regionsList = new List<Region>
             //{
             //    new Region { Id = Guid.NewGuid(), Name = "AukLand Region", Code = "AKL", RegionImageUrl = "https://picsum.photos/536/354" },
             //    new Region{ Id= Guid.NewGuid(), Name="Willington", Code="WLT", RegionImageUrl="https://picsum.photos/id/16/367/267" }
             //};
-
             return Ok(regionDto);
         }
 
@@ -48,8 +46,8 @@ namespace NZWalkAPI.Controllers
         [Route("{id:guid}")]
         public IActionResult GetRegionById([FromRoute] Guid id)
         {
-            var region = _db.Regions.FirstOrDefault(x=>x.Id==id);
-            if (region == null) 
+            var region = _db.Regions.FirstOrDefault(x => x.Id == id);
+            if (region == null)
             {
                 return NotFound();
             }
@@ -61,10 +59,69 @@ namespace NZWalkAPI.Controllers
                     Code = region.Code,
                     ImageUrl = region.RegionImageUrl
                 };
-                return Ok(regionDto);  
-                    
+                return Ok(regionDto);
             }
-            
         }
+
+        [HttpPost]
+        public IActionResult CreateRegion([FromBody] AddRegionDTO addRegionDTO)
+        {
+            if (addRegionDTO != null)
+            {
+                var regionModel = new Region
+                {
+                    Name = addRegionDTO.Name,
+                    Code = addRegionDTO.Code,
+                    RegionImageUrl = addRegionDTO.RegionImageUrl
+                };
+                _db.Regions.Add(regionModel);
+                _db.SaveChanges();
+                var regDTO = new RegionDTO()
+                {
+                    Id = regionModel.Id,
+                    RegionName = regionModel.Name,
+                    Code = regionModel.Code,
+                    ImageUrl = regionModel.RegionImageUrl
+                };
+                //return Ok();
+                Guid regModelId = regionModel.Id;
+                Guid regDTOId = regDTO.Id;
+                return CreatedAtAction(nameof(GetRegionById), new { id = regDTO.Id }, regDTO);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("{id:guid}") ]
+        public  IActionResult UpdateRegion([FromRoute]Guid id, [FromBody]AddRegionDTO updateRegDTO)
+        {
+            var region = _db.Regions.FirstOrDefault(x=> x.Id==id);
+            if (region!= null)
+            {
+                //Updating the data that is retrived from the DB.
+                region.Name = updateRegDTO.Name;
+                region.Code = updateRegDTO.Code;
+                region.RegionImageUrl = updateRegDTO.RegionImageUrl;               
+                _db.Regions.Update(region);
+                _db.SaveChanges();
+                var regDTO = new AddRegionDTO
+                {
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl
+                };
+                return Ok(regDTO);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+
     }
 }
---
+++ NZWalkAPI/Models/DTOs/AddRegionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace NZWalkAPI.Models.DTOs
+{
+    public class AddRegionDTO
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
