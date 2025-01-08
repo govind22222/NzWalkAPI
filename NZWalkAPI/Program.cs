08a2fbc4064d51f1c5bf7522ce946a3094baaf8e
raghvendra-imarc <raghvendra.singh@imarcgroup.com>
2025-01-08 17:08:14 +0530

    Set Identity for user role,Authentication & authorization, Password, and to generate Token in program.cs on 08-Jan-25 Lecture-62.

--- NZWalkAPI/Program.cs
+++ NZWalkAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using NZWalkAPI.DB;
@@ -12,11 +13,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Added AppdbContext to the Services by providing the connection string, So that it can be used at many controller by DI.
 //Basically below line is adding the AppDBContext to the services and providing the connection string to the AppDBContext.
-builder.Services.AddDbContext<AppDBContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("NzConnStr")));
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NzConnStr")));
 
 //Added other Database for user Role and Credential.
-builder.Services.AddDbContext<AuthAppDBContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("NzAuthDBConnStr")));
+builder.Services.AddDbContext<AuthAppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NzAuthDBConnStr")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -39,8 +39,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         ValidateIssuerSigningKey = true,
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
         ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey= new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
     });
+
+//--------Setting Identity along with JWT Authentication---------
+builder.Services.AddIdentityCore<IdentityUser>()  //Enables core identity functionality 
+    .AddRoles<IdentityRole>()    //Enables role-based authorization like- "User.IsInRole("Admin")"
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")    //Adds a custom token provider for generating and validating tokens
+    .AddEntityFrameworkStores<AuthAppDBContext>()       //Representing the database where user and role information will be stored.
+    .AddDefaultTokenProviders();   // Generate tokens for Email Confirmation(A token for verifying user email addresses), Password Reset, Phone Number Confirmation.
+
+//Configuring password
+builder.Services.Configure<IdentityOptions>(options =>
+{
+
+});
+
 
 var app = builder.Build();
 
