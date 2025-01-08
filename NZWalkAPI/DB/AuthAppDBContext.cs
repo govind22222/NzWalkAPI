1b399261f3674940b1b71d1309a627d67607d76f
raghvendra-imarc <raghvendra.singh@imarcgroup.com>
2025-01-08 16:01:50 +0530

    Seeded Roles to the AuthAppDBContext.cs on 08-Jan-25, Lecture-60.

--- NZWalkAPI/DB/AuthAppDBContext.cs
+++ NZWalkAPI/DB/AuthAppDBContext.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalkAPI.DB
 {
-    public class AuthAppDBContext :IdentityDbContext
+    public class AuthAppDBContext : IdentityDbContext
     {
-        public AuthAppDBContext( DbContextOptions<AuthAppDBContext> options): base(options)
+        public AuthAppDBContext(DbContextOptions<AuthAppDBContext> options) : base(options)
         {
-            
         }
+
+        //Seeding Roles to DB by Raghav on 08-Jan-25.
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            var writeRole = "af962303-b468-423d-bdf4-7d8589e94687";
+            var readRole = "b4e0b098-c394-495d-88e4-3420711577b9";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Id=writeRole,
+                    Name="WriteRole",
+                    NormalizedName= "writer".ToUpper(),
+                    ConcurrencyStamp =writeRole
+                },
+                new IdentityRole
+                {
+                    Id=readRole,
+                    Name="Reader",
+                    NormalizedName="Reader".ToUpper(),
+                    ConcurrencyStamp=readRole
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+
     }
 }
